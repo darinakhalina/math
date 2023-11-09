9e46a767eafe1a84ec4acda1a25c1bc2518e5941
Darina Khalina <darina.khalina@namecheap.com>
2023-11-09 15:34:35 +0200

    ex

---
+++ links.py
@@ -0,0 +1,4 @@
+# https://www.youtube.com/playlist?list=PLZHQObOWTQDPD3MizzM2xVFitgF8hE_ab
+
+
+# https://colab.research.google.com/drive/1xvj-MnGX3LJNR39wwbw5R6S3cSehFoSn#scrollTo=vD1QR0H8Zq75
--- t-1.py
+++ t-1.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+# https://colab.research.google.com/drive/1xvj-MnGX3LJNR39wwbw5R6S3cSehFoSn#scrollTo=vD1QR0H8Zq75
+
 # Вектор-столбец
 vector_column = np.array([[1],
                           [2],
@@ -8,7 +10,34 @@ vector_column = np.array([[1],
 
 print("Вектор-столбец:", vector_column)
 
+a = np.array([ 2,  1,  -1])
+
+print(a)
+
 # Вектор-рядок
 vector_row = np.array([1, 2, 3, 4])
 
 print("Вектор-рядок:", vector_row)
+
+
+b = np.array([ [2], [1], [-1]])
+
+print(b)
+
+# Матриця
+import numpy as np
+
+A = np.array([ [2, 1, -1], [0, 1, -4] ])
+
+print("матриця:",A)
+
+B = np.array([ [5, 5, 5], [5, 5, 5],[5, 5, 5] ])
+
+print(B)
+
+
+# toDo: remove
+# С = A / B, 
+# np.divide
+# А = np.array([[1], [2], [3], [4], [5], [6], [7], [8]]).
+# 8й пункт із ДЗ 1 можна пропустити
