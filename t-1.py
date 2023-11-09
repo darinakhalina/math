17007090dcae378509a9b7e9425e12214d96b73f
Darina Khalina <darina.khalina@namecheap.com>
2023-11-09 14:49:15 +0200

    add ex

---
+++ t-1.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Вектор-столбец
+vector_column = np.array([[1],
+                          [2],
+                          [3],
+                          [4]])
+
+print("Вектор-столбец:", vector_column)
+
+# Вектор-рядок
+vector_row = np.array([1, 2, 3, 4])
+
+print("Вектор-рядок:", vector_row)
