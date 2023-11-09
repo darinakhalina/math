dc3b7d0e03e2cae132aa944d32e0298668ba3701
Darina Khalina <darina.khalina@namecheap.com>
2023-11-09 21:46:09 +0200

    ex

--- links.py
+++ links.py
@@ -4,3 +4,10 @@
 # https://colab.research.google.com/drive/1xvj-MnGX3LJNR39wwbw5R6S3cSehFoSn#scrollTo=vD1QR0H8Zq75
 
 # https://www.youtube.com/watch?v=WAhTHKCo0OQ
+# https://colab.research.google.com/drive/1ze3iEQdShyrYtNznxY0iB1KOljdWMqPd?usp=sharing
+# https://colab.research.google.com/drive/1-se5poiJ4a-P5mXfvqpys2SIg8f7umnW?usp=sharing#scrollTo=ajIp3eGqjwDG&uniqifier=2
+# 
+#
+#
+#
+#
