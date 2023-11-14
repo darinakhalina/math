15ad1ee6fbba133a09e49e506abb9f1942e3e271
Darina Khalina <darina.khalina@namecheap.com>
2023-11-14 20:13:27 +0200

    уч

--- links.py
+++ links.py
@@ -7,7 +7,7 @@
 # https://colab.research.google.com/drive/1ze3iEQdShyrYtNznxY0iB1KOljdWMqPd?usp=sharing
 # https://colab.research.google.com/drive/1-se5poiJ4a-P5mXfvqpys2SIg8f7umnW?usp=sharing#scrollTo=ajIp3eGqjwDG&uniqifier=2
 # https://github.com/mikh-maksi/math-code/blob/main/math-coord/lesson03/tk_coord.py
-#
-#
+# https://onlinemschool.com/math/assistance/vector/p_to_vector/
+# https://www.intmath.com/vectors/1-vector-concepts.php
 #
 #
