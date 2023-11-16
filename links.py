93e446da13b86f6600a33a0acbf82ee0e380b4cd
Darina Khalina <darina.khalina@namecheap.com>
2023-11-16 20:15:08 +0200

    111

--- links.py
+++ links.py
@@ -13,6 +13,6 @@
 # https://ua.onlinemschool.com/math/assistance/vector/p_to_vector/
 # https://github.com/mikh-maksi/math-code/blob/main/math-coord/lesson03/task_tk_07.py
 # https://colab.research.google.com/drive/1ja3lNfpWhQySMJ7cs9dtkV1SR-eFSrEX#scrollTo=Aj4l2c0ebi3F - пример рисунка с векторами
-#
+# https://colab.research.google.com/drive/1EJ3yke4QLOrX9cQNWFv5faV2EQiwRqIN
 #
 #
