2beb08468b6c2dfba819d50883384f1438df1d59
Darina Khalina <darina.khalina@namecheap.com>
2023-12-03 23:11:39 +0200

    ex

--- links.py
+++ links.py
@@ -57,7 +57,7 @@
 # задачи https://moodle.znu.edu.ua/pluginfile.php/1113833/mod_resource/content/2/%D0%9B%D0%B5%D0%BA%D1%86%D1%96%D1%8F%207_%D0%9F%D1%80%D0%B0%D0%BA%D1%82%D0%B8%D1%87%D0%BD%D0%B5%20%D0%B7%D0%B0%D0%BD%D1%8F%D1%82%D1%82%D1%8F%207_%D0%9C%D0%B5%D1%82%D0%BE%D0%B4%D0%B8%20%D0%BA%D0%BE%D0%BC%D0%B1%D1%96%D0%BD%D0%B0%D1%82%D0%BE%D1%80%D0%BD%D0%BE%D0%B3%D0%BE%20%D0%B0%D0%BD%D0%B0%D0%BB%D1%96%D0%B7%D1%83.pdf
 # задачи https://openarchive.nure.ua/server/api/core/bitstreams/649872d8-4d2c-4d94-b0b9-d01c275aba3f/content
 # 
-#
+# https://www.wolframalpha.com/
 #
 #
 #
